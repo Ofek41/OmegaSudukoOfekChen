78174e38677f2288b23c8352f509831e73595810
Ofek41 <ofekchen2006@gmail.com>
2025-01-09 18:39:20 +0200

    Added new module that deals with strings and boards and converts from one to another.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 {
     internal class Program
     {
+        public static int N; // N represents the dimension of the suduko board.
+        public static string boardString; // A string that represents the suduko board.
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
---
+++ StringAndBoard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OmegaSuduko.Program;
+
+namespace OmegaSuduko
+{
+    internal class StringAndBoard
+    {
+        // This function converts the suduko board's string to the board itself.
+        public static int[,] StringToBoard()
+        {
+            int[,] sudukoBoard = new int[N, N];
+            for (int i = 0; i < N*N; i++)
+            {
+                sudukoBoard[i / N, i % N] = boardString[i] - '0';
+            }
+            return sudukoBoard;
+        }
+
+        // This function converts the suduko board to the string that represents it.
+        public static string BoardToString(int[,] sudukoBoard)
+        {
+            string sudukoString = "";
+            for (int i =0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    sudukoString += sudukoBoard[i, j].ToString();
+                }
+            }
+            return sudukoString;
+        }
+    }
+}
