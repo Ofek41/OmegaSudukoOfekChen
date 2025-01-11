eb9d607d4301792c461842e7455c4a36d10b165e
Ofek41 <ofekchen2006@gmail.com>
2025-01-11 12:48:03 +0200

    Added 3 functions to check if a number exists in the row, column or inner box of the suduko board matrix

--- Filtering.cs
+++ Filtering.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,6 +35,61 @@ namespace OmegaSuduko
                         hashBoard[i, j] = new HashSet<int>(Enumerable.Range(1, N));
                 }
             }
+        }
+
+        /// <summary>
+        /// This function checks if the number exists in the row.
+        /// </summary>
+        /// <param name="row">the row we are currently checking</param>
+        /// <param name="number">the number we check if exists in row</param>
+        /// <returns>returns true if the number exists in the row and false if not</returns>
+        public static bool ExistsInRow(int row, int number)
+        {
+            for (int column = 0; column < N; column++)
+            {
+                if (sudukoBoard[row, column] == number)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// This function checks if the number exists in the column.
+        /// </summary>
+        /// <param name="column">the column we are currently checking</param>
+        /// <param name="number">the number we check if exists in row</param>
+        /// <returns>returns true if the number exists in the column and false if not</returns>
+        public static bool ExistsInColumn(int column, int number)
+        {
+            for (int row = 0; row < N; row++)
+            {
+                if (sudukoBoard[row, column] == number)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// This function checks if the number exists in the inner square of the suduko board
+        /// </summary>
+        /// <param name="row">the row we are currently checking</param>
+        /// <param name="column">the column we are currently checking</param>
+        /// <param name="number">the number we check if exists in the square</param>
+        /// <returns>returns true if the number exists in the inner square and false if not</returns>
+        public static bool ExistsInInnerSquare(int row, int column, int number)
+        {
+            int squareSize = (int)Math.Sqrt(N);
+            int rowSquareBegin = (row / squareSize) * squareSize;
+            int columnSquareBegin = (column / squareSize) * squareSize;
+            for (int i = 0; i < squareSize; i++)
+            {
+                for (int j = 0; j < squareSize; j++)
+                {
+                    if (sudukoBoard[rowSquareBegin + i, columnSquareBegin + j] == number)
+                        return true;
+                }
+            }
+            return false;
         }
     }
 }
